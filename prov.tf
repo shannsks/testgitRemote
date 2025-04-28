eb3ed43af8224de0ee70574dae1095e5a8cb1df1
shannsks <shannu.sks@gmail.com>
2025-04-28 03:18:18 -0400

    storage added

--- prov.tf
+++ prov.tf
@@ -23,3 +23,11 @@ resource "azurerm_resource_group" "testgt" {
   location = "East us"
 
 }
+
+resource "azurerm_storage_account" "testgt" {
+  name = "stgshan888"
+  resource_group_name = "test_rg"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  
+}
